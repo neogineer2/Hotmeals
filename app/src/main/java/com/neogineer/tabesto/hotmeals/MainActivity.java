4c88595faa0502361d353b3b569bbcf1e5016b80
Achraf <achraf.amil@orange.com>
2018-06-27 19:28:43 +0200

    first commit

---
+++ app/src/main/java/com/neogineer/tabesto/hotmeals/MainActivity.java
@@ -0,0 +1,19 @@
+package com.neogineer.tabesto.hotmeals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recycler = findViewById(R.id.recycler_view);
+
+    }
+}
